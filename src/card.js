f0c188a6cfb262efb140122b1949131795423fc4
shashankkestwal <shashankkestwal777@gmail.com>
2022-12-14 23:34:24 +0530

    added carousal

--- src/card.js
+++ src/card.js
@@ -3,6 +3,7 @@ import './card.css'
 import Image from './image.js'
 import x from './assets/sitting_person.svg'
 
+
 export default class card extends react.Component {
   static defaultProps = {
     img : x, 
@@ -14,7 +15,7 @@ export default class card extends react.Component {
       <div className="skill_card"> 
         <Image source = {this.props.img} />
         <p> {this.props.txt} </p>
-        <a href='#'>link</a>
+        {/*<a href='#'>link</a>*/}
       </div>
     )
   };
--- src/skills.js
+++ src/skills.js
@@ -2,16 +2,26 @@ import react from 'react'
 import './skills.css'
 import Card from './card.js'
 import img from './assets/standing_person.png'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 export default class skills extends react.Component {
   render (){
     return (
         <div className="skill_cards_container">
-          <Card img={img}/>
-          <Card />
-          <Card/>
-          <Card/>
-        </div>
+          <Carousel autoPlay={true}
+                  emulateTouch={true}
+                  infiniteLoop={true}
+                  interval={1800}
+                  stopOnHover={true}
+                  showThumbs={false}
+                  showStatus={false}
+                  showIndicators={false}>
+                <Card img = {img}/>
+                <Card/>
+                <Card/>
+          </Carousel>
+        </div>         
     )
   };
 }
